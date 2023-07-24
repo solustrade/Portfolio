84be4413dda99e6a6aee542fd7275d68a995f762
Helcio Silva <comercial@psbios.com>
2023-07-24 15:01:16 -0300

    Imprimir o conteúdo de um ListBox
    Imprimir o conteúdo de um ListBox.

---
+++ C#/Imprimir_ListBox/Imprimir_ListBox/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace Imprimir_ListBox
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbDados = new System.Windows.Forms.ListBox();
+            this.btnCarregarDados = new System.Windows.Forms.Button();
+            this.btnImprimir = new System.Windows.Forms.Button();
+            this.pd1 = new System.Drawing.Printing.PrintDocument();
+            this.printDialog1 = new System.Windows.Forms.PrintDialog();
+            this.chkBD = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // lbDados
+            // 
+            this.lbDados.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbDados.ForeColor = System.Drawing.Color.Blue;
+            this.lbDados.FormattingEnabled = true;
+            this.lbDados.ItemHeight = 16;
+            this.lbDados.Location = new System.Drawing.Point(26, 22);
+            this.lbDados.Name = "lbDados";
+            this.lbDados.Size = new System.Drawing.Size(371, 292);
+            this.lbDados.TabIndex = 0;
+            // 
+            // btnCarregarDados
+            // 
+            this.btnCarregarDados.Enabled = false;
+            this.btnCarregarDados.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCarregarDados.Location = new System.Drawing.Point(26, 331);
+            this.btnCarregarDados.Name = "btnCarregarDados";
+            this.btnCarregarDados.Size = new System.Drawing.Size(129, 51);
+            this.btnCarregarDados.TabIndex = 1;
+            this.btnCarregarDados.Text = "Carregar Dados ";
+            this.btnCarregarDados.UseVisualStyleBackColor = true;
+            this.btnCarregarDados.Click += new System.EventHandler(this.btnCarregarDados_Click);
+            // 
+            // btnImprimir
+            // 
+            this.btnImprimir.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnImprimir.Location = new System.Drawing.Point(268, 331);
+            this.btnImprimir.Name = "btnImprimir";
+            this.btnImprimir.Size = new System.Drawing.Size(129, 51);
+            this.btnImprimir.TabIndex = 1;
+            this.btnImprimir.Text = "Imprimir";
+            this.btnImprimir.UseVisualStyleBackColor = true;
+            this.btnImprimir.Click += new System.EventHandler(this.btnImprimir_Click);
+            // 
+            // pd1
+            // 
+            this.pd1.PrintPage += new System.Drawing.Printing.PrintPageEventHandler(this.pd1_PrintPage);
+            // 
+            // printDialog1
+            // 
+            this.printDialog1.UseEXDialog = true;
+            // 
+            // chkBD
+            // 
+            this.chkBD.AutoSize = true;
+            this.chkBD.Location = new System.Drawing.Point(172, 349);
+            this.chkBD.Name = "chkBD";
+            this.chkBD.Size = new System.Drawing.Size(71, 17);
+            this.chkBD.TabIndex = 2;
+            this.chkBD.Text = "Fonte BD";
+            this.chkBD.UseVisualStyleBackColor = true;
+            this.chkBD.CheckedChanged += new System.EventHandler(this.chkBD_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(429, 391);
+            this.Controls.Add(this.chkBD);
+            this.Controls.Add(this.btnImprimir);
+            this.Controls.Add(this.btnCarregarDados);
+            this.Controls.Add(this.lbDados);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Imprimir ListBox";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lbDados;
+        private System.Windows.Forms.Button btnCarregarDados;
+        private System.Windows.Forms.Button btnImprimir;
+        private System.Drawing.Printing.PrintDocument pd1;
+        private System.Windows.Forms.PrintDialog printDialog1;
+        private System.Windows.Forms.CheckBox chkBD;
+    }
+}
+
---
+++ C#/Imprimir_ListBox/Imprimir_ListBox/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+using System.IO;
+using System.Drawing;
+
+namespace Imprimir_ListBox
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private StringReader meuLeitor;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string[] nomes = new string[12];
+
+            nomes[0] = "Macoratti";
+            nomes[1] = "Jefferson";
+            nomes[2] = "Janice";
+            nomes[3] = "Jessica";
+            nomes[4] = "Miriam";
+            nomes[5] = "Marcia";
+            nomes[6] = "Irene";
+            nomes[7] = "Yuri";
+            nomes[8] = "Bianca";
+            nomes[9] = "Igor";
+            nomes[10] = "Larissa";
+            nomes[11] = "Giovana";
+
+            lbDados.Items.AddRange(nomes);
+        }
+
+        private void btnCarregarDados_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SqlConnection conn = new SqlConnection(@"Data Source=.\SQLEXPRESS;Initial Catalog=Northwind;Integrated Security=True");
+                conn.Open();
+                DataSet ds = new DataSet();
+                SqlDataAdapter adapter = new SqlDataAdapter("SELECT ProductId, ProductName from Products", conn);
+                adapter.Fill(ds, "Products");
+                lbDados.ValueMember = "ProductId";
+                lbDados.DisplayMember = "ProductName";
+                lbDados.DataSource = ds.Tables["Products"];
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro " + ex.Message, "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void pd1_PrintPage(object sender, System.Drawing.Printing.PrintPageEventArgs e)
+        {
+            //define as variáveis para controlar as linhas, o posicionamento e a caneta e cor usada
+            float linhasPorPagina = 0;
+            float yPosicao = 0;
+            int contador = 0;
+            float MargemEsquerda = e.MarginBounds.Left;
+            float MargemTopo = e.MarginBounds.Top;
+            string linha = null;
+
+            // define a fonte e a pena e sua cor
+            Font FonteImpressao = this.lbDados.Font;
+            SolidBrush minhaPena = new SolidBrush(Color.Black);
+         
+            // Define o numero de linhas por pagina usando MarginBounds.
+            linhasPorPagina = e.MarginBounds.Height / FonteImpressao.GetHeight(e.Graphics);
+
+            // Percorre a string usando o StringReadere imprime cada linha
+            while (contador < linhasPorPagina && ((linha = meuLeitor.ReadLine()) != null))
+           {
+                // calcula a posição da proxima linha com base na
+                // altura da fonte de acordo com o dispositivo de impressão
+                yPosicao = MargemTopo + (contador * FonteImpressao.GetHeight(e.Graphics));
+                // desenha a proxim alinha no controle 
+                e.Graphics.DrawString(linha, FonteImpressao, minhaPena, MargemEsquerda, yPosicao, new StringFormat());
+                contador++;
+            }
+
+            // Se existe mais linhas imprime outra pagina
+            if (linha != null)
+                e.HasMorePages = true;
+            else
+                e.HasMorePages = false;
+                minhaPena.Dispose();
+       }
+
+        private void btnImprimir_Click(object sender, EventArgs e)
+        {
+            printDialog1.Document = pd1;
+            string strTexto = "";
+
+            if (chkBD.Checked)
+            {
+                for (int i = 0; i < lbDados.Items.Count-1; i++)
+                {
+                        DataRowView drv = (DataRowView)lbDados.Items[i];
+                        String elemento = drv["ProductName"].ToString();
+                        strTexto = strTexto + elemento.ToString() + "\n";
+                }
+            }
+            else
+            {
+                foreach (object x in lbDados.Items)
+                {
+                        strTexto = strTexto + x.ToString() + "\n";
+                }
+            }
+         
+            meuLeitor = new StringReader(strTexto);
+
+            if (printDialog1.ShowDialog() == DialogResult.OK)
+            {
+                 this.pd1.Print();
+            }
+        }
+
+        private void chkBD_CheckedChanged(object sender, EventArgs e)
+        {
+
+            btnCarregarDados.Enabled = chkBD.Checked;
+        }
+    }
+}
